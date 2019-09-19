bc156eabf68870a4b5538aabbec92e6c4dd8df86
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-09-19 20:01:38 +0200

    Clean the day 1 challenge files

--- js/day1.js
+++ js/day1.js
@@ -1,17 +1,26 @@
-let drumKit = () => {
-    function playSound(e) {
-        const area = document.querySelector("#day-1");
-        const audio = area.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const key = area.querySelector(`.key[data-key="${e.keyCode}"]`);
-        if (!audio) return; // stop the function if there is no corresponding audio
-        audio.currentTime = 0; // rewind to start
-        audio.play();
-        key.classList.add("playing");
-    }
+let drumKit = () => window.addEventListener('keydown', playDrum);
 
-    window.addEventListener('keydown', playSound);
+let playDrum = (e) => {
+    const area = document.querySelector("#day-1");
+    const audio = area.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = area.querySelector(`.key[data-key="${e.keyCode}"]`);
+    playAudio(audio);
+    animateKey(key);
+}
 
-    // the rest of the function is in <script> in html files
+let playAudio = (audio) => {
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+}
+
+let animateKey = (key) => {
+    key.classList.add("playing");
+    key.addEventListener('transitionend', removeTransition);
+}
+
+let removeTransition = (action) => {
+    action.srcElement.classList.remove('playing');
 }
 
 export { drumKit }
