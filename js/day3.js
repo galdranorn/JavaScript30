144084d14b774402782dbb2572626531f832bfd6
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-09-21 10:38:14 +0200

    Clean the day 3 challenge files

---
+++ js/day3.js
@@ -0,0 +1,18 @@
+let startCssUpdater = () => {
+    window.addEventListener('click', addInputListeners)
+}
+
+let addInputListeners = () => {
+    const inputs = Array.from(document.querySelectorAll('.controls input'));
+    inputs.forEach(input => input.addEventListener('change', handleInputUpdate));
+    inputs.forEach(input => input.addEventListener('mousemove', handleInputUpdate));
+}
+
+function handleInputUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+export { startCssUpdater }
+
+
--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-import { renderStructure } from './structure.js';
-import { playDrums } from './day1.js';
-import { startClock } from './day2.js';
+import { renderStructure } from './structure.js'
+import { playDrums } from './day1.js'
+import { startClock } from './day2.js'
+import { startCssUpdater } from './day3.js'
 
 renderStructure(30);
 
 playDrums();
 startClock();
+startCssUpdater();
 
