d7e1409b842f0913b3dd5909e23adeb3f77b7633
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-09-23 17:37:47 +0200

    Finish day 8 challenge - add hue and width changes to canvas

--- js/challenges/day8.js
+++ js/challenges/day8.js
@@ -3,7 +3,9 @@ let canvas = () => {
 }
 
 let isDrawing = false;
-let lastPosition;
+let lastPosition = [];
+let hue = 0;
+let direction = true;
 
 let setCanvas = () => {
     const area = document.querySelector("#day-8");
@@ -11,17 +13,23 @@ let setCanvas = () => {
     const ctx = canvas.getContext("2d");
     canvas.width = area.offsetWidth;
     canvas.height = area.offsetHeight;
-    ctx.strokeStyle = "#BADA55";
+    ctx.lineWidth = 50;
     ctx.lineJoin = "round";
     ctx.lineCap = "round";
+    
 
     let draw = (e) => {
         if (isDrawing) {
+            ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
             ctx.beginPath()
             ctx.moveTo(...lastPosition)
             ctx.lineTo(e.offsetX, e.offsetY)
             ctx.stroke()
-            lastPosition = [ e.offsetX, e.offsetY ] 
+
+            lastPosition = [ e.offsetX, e.offsetY ];
+            (hue >= 360) ? hue = 0 : hue++;
+            (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) ? direction = !direction : direction;
+            (direction) ? ctx.lineWidth++ : ctx.lineWidth--;
         }
     }
 
@@ -31,7 +39,6 @@ let setCanvas = () => {
     canvas.addEventListener("mouseout", () => isDrawing = false)
 
     window.removeEventListener('click', setCanvas)
-
 }
 
 export { canvas }
